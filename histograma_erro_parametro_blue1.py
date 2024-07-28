43c50414d4653c63be85158140ec0c888a2952f5
MorettGuilherme <guilherme.barroso.morett@cern.ch>
2024-07-28 19:21:57 -0300

    ninth commit

--- histograma_erro_parametro_blue1.py
+++ histograma_erro_parametro_blue1.py
@@ -72,7 +72,7 @@ def dados_estatisticos_erro_estimacao_parametro_BLUE1(lista_erro_estimacao_param
     
 ### -------------------------------------------------------------------------------------------------------------------------------------------- ###
 
-### ------------ 2) FUNÇÃO PARA A CONSTRUÇÃO DO HISTOGRAMA DO ERRO DE ESTIMAÇÃO DA AMPLITUDE, FASE OU PEDESTAL PELO MÉTODO BLUE1 --------------- ###
+### ------- 2) INSTRUÇÃO PARA A CONSTRUÇÃO DO HISTOGRAMA DO TIPO A DO ERRO DE ESTIMAÇÃO DA AMPLITUDE, FASE OU PEDESTAL PELO MÉTODO BLUE1 ------- ###
 
 # Definição de função para o plot do histograma do tipo A do erro de estimação da amplitude, fase ou pedestal pelo método BLUE1.
 def histograma_A_erro_estimacao_parametro_BLUE1(n_ocupacao, parametro, lista_erro_estimacao_parametro, media_erro_estimacao_parametro, var_erro_estimacao_parametro, desvio_padrao_erro_estimacao_parametro):
@@ -152,7 +152,7 @@ def histograma_A_erro_estimacao_parametro_BLUE1(n_ocupacao, parametro, lista_err
 
 ### -------------------------------------------------------------------------------------------------------------------------------------------- ###
 
-### ------ 3) FUNÇÃO PARA A CONSTRUÇÃO DO HISTOGRAMA DO TIPO B DO ERRO DE ESTIMAÇÃO DA AMPLITUDE, FASE OU PEDESTAL PELO MÉTODO BLUE1 ----------- ###
+### ------ 3) INSTRUÇÃO PARA A CONSTRUÇÃO DO HISTOGRAMA DO TIPO B DO ERRO DE ESTIMAÇÃO DA AMPLITUDE, FASE OU PEDESTAL PELO MÉTODO BLUE1 -------- ###
 
 # Definição de função para o plot dos histogramas do tipo B do erro de estimação da amplitude, fase ou pedestal para diferentes janelamentos para uma dada ocupação pelo método BLUE1.
 def histograma_B_erro_estimacao_parametro_BLUE1(n_ocupacao, parametro, lista_erro_estimacao_parametro_J7, media_erro_estimacao_parametro_J7, var_erro_estimacao_parametro_J7, desvio_padrao_erro_estimacao_parametro_J7, lista_erro_estimacao_parametro_J15, media_erro_estimacao_parametro_J15, var_erro_estimacao_parametro_J15, desvio_padrao_erro_estimacao_parametro_J15, lista_erro_estimacao_parametro_J19, media_erro_estimacao_parametro_J19, var_erro_estimacao_parametro_J19, desvio_padrao_erro_estimacao_parametro_J19):
